a3000d301acc7cf78f124106b22a409bfc6c421c
Firmaplus(+) Ltd. <w.stubig@firmaplus.de>
2025-11-23 00:02:23 +0100

    Untested 7800 Keyboard updates

--- src/protocols/ProtocolAtari7800Keyboard.cpp
+++ src/protocols/ProtocolAtari7800Keyboard.cpp
@@ -3,6 +3,36 @@
 
 #include "ProtocolAtari7800Keyboard.h"
 
+#define COM_RETURN             0b111
+#define COM_SCAN_KEYBOARD      0b000
+#define COM_SIO_OPERATION      0b001
+#define COM_PROGRAM_RECORDER   0b010
+#define COM_CASSETTE_RECORDER  0b011
+
+ProtocolAtari7800Keyboard::Node ProtocolAtari7800Keyboard::nodes[] = {
+  /* 0 */ { 0, true, {  1, 2, 3, 4,-1,-1,-1 }, nullptr },           // ROOT
+
+  // ROOT folder commands and subfolders
+  /* 1 */ { 0, true, {  5, 6,-1,-1,-1,-1,-1 }, nullptr },           // KEYBOARD-SCAN
+  /* 2 */ { 0, true, { -1,-1,-1,-1,-1,-1,-1 }, nullptr },           // SIO-OPERATIONS
+  /* 3 */ { 0, true, { -1,-1,-1,-1,-1,-1,-1 }, nullptr },           // PROGRAM RECORDER
+  /* 4 */ { 0, true, { -1,-1,-1,-1,-1,-1,-1 }, nullptr },           // CASSETTE RECORDER
+
+  // KEYBOARD commands and subfolders
+  /* 4 */ { 1, false, { -1,-1,-1,-1,-1,-1,-1 }, &ProtocolAtari7800Keyboard::cmd_GET_KEY },   // GET-KEY (Command)
+  /* 5 */ { 1, false, { -1,-1,-1,-1,-1,-1,-1 }, &ProtocolAtari7800Keyboard::cmd_GET_STATE }  // GET-STATE (Command)
+
+};
+
+void ProtocolAtari7800Keyboard::cmd_GET_KEY() {
+  sendResponse(my_current_keycode);
+}
+void ProtocolAtari7800Keyboard::cmd_GET_STATE() { sendResponse(0x00); }
+
+void ProtocolAtari7800Keyboard::sendResponse(uint8_t value) {
+
+}
+
 void ProtocolAtari7800Keyboard::setup() {
   // disable joystick port forwarding (left)
   digitalWrite(OE_LEFT_PORT, LOW);
@@ -12,8 +42,8 @@ void ProtocolAtari7800Keyboard::setup() {
   PORTD = 0b00000000; // inputs no pullups, outputs high
 //  PORTD = 0b11111111; // activate pullups
 
-  pinMode(INPT_5, OUTPUT);
-  digitalWrite(INPT_5, HIGH); // KCLK
+  pinMode(KCLK, OUTPUT);
+  digitalWrite(KCLK, HIGH); // KCLK
 
   cli();                    // globale Interrupts aus
   PCICR |= _BV(PCIE2);      // Pin-Change-Interrupts auf Port D aktivieren
@@ -31,9 +61,37 @@ void ProtocolAtari7800Keyboard::keyUp() {
 }
 
 void ProtocolAtari7800Keyboard::loop() {
-    // Periodische Logik für ProtoA
+  if (current_command != 0xff){
+    if (current_command == 0b111){
+      current_node = nodes[current_node].parent;
+    } else {
+      uint8_t new_node = nodes[current_node].children[current_command];
+      if (new_node == -1){
+        // Not implemented yet!!
+      } else if (nodes[new_node].isFolder){
+        current_node = new_node;
+      } else {
+        // Base-Unit really want something from us now..
+        (this->*nodes[new_node].handler)();
+      }
+    }
+
+    PINB = (1 << PB3);  // Toggle Bit 3 of Port B (KCLK) to tell Base-Unit we are done and ready.
+    current_command = 0xff;
+  }
 }
 
 void ProtocolAtari7800Keyboard::pISR() {
-    // ISR-spezifische Logik für ProtoA
+  // Base-Unit toggled BCLK
+  switch (my_transfer_state){
+    case WAIT_FOR_CMD:
+      current_command = PIND & 0b111;
+      break;
+    case TRANSFER_BYTE_BIT3_5:
+      my_transfer_state = TRANSFER_BYTE_BIT6_7;
+      break;
+    case TRANSFER_BYTE_BIT6_7:
+      my_transfer_state = WAIT_FOR_CMD;
+      break;
+  }
 }
--- src/protocols/ProtocolAtari7800Keyboard.h
+++ src/protocols/ProtocolAtari7800Keyboard.h
@@ -3,10 +3,36 @@
 #include "Protocol.h"
 #include "../charsets/Atari7800Keyboard.h"
 
+//typedef void (*CmdHandler)(void);
+
 class ProtocolAtari7800Keyboard : public Protocol {
   private:
+
+    typedef void (ProtocolAtari7800Keyboard::*CmdHandler)(void);
+
+    enum TxStates {
+      WAIT_FOR_CMD,
+      TRANSFER_BYTE_BIT0_2,
+      TRANSFER_BYTE_BIT3_5,
+      TRANSFER_BYTE_BIT6_7
+    };
+
+    typedef struct {
+      uint8_t parent;
+      bool isFolder;
+      uint8_t children[7];   // 0..6 Childs
+      CmdHandler handler;   // != NULL -> Command
+    } Node;
+
+    uint8_t current_command = 0xff;
+    uint8_t current_node = 0;
+    TxStates my_transfer_state = WAIT_FOR_CMD;
+    static Node nodes[];
     volatile uint8_t my_current_keycode;
+    void sendResponse(uint8_t value);
   public:
+    void cmd_GET_KEY();
+    void cmd_GET_STATE();
     void setup() override;
     void keyDown(char value) override;
     void keyUp() override;
